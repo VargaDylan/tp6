e1f918d15bf2770d9533e1277b5002bea7c8621b
Dylan Varga <vargadylan92@gmail.com>
2021-10-11 19:31:19 -0300

    ya entendi en la esepcion que puse que se ejecuta cuando pasan el cursor sobre el boton borrar le puse que me limpie el cuadro de texto esa parte estaba fuera del try entonces me boraba el texto antes de verificarlo  trabajo terminado

--- src/trabajon6/VistaBorrarCliente.java
+++ src/trabajon6/VistaBorrarCliente.java
@@ -155,14 +155,16 @@ public class VistaBorrarCliente extends javax.swing.JInternalFrame {
     private void jbBorrarMouseMoved(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jbBorrarMouseMoved
         // TODO add your handling code here:
       
-            /*  try{
+         try{
           int a=Integer.parseInt(jtDniBorrar.getText());
       }catch(NumberFormatException nf){
           JOptionPane.showMessageDialog(this, "por favor ingrese un numero valido SIN LETRAS");
           jtDniBorrar.requestFocus();
+       jtDniBorrar.setText("");
+      
       }
        
-        jtDniBorrar.setText("");*/
+       
     }//GEN-LAST:event_jbBorrarMouseMoved
 
 
